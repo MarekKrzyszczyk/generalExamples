ee90fb657292310df5161655f8f4fb902042ace8
Marek Krzyszczyk <marek.krzyszczyk@wp.pl>
2019-10-26 22:06:03 +0200

    Ex04 General

--- src/test/java/com/sda/general/ex04/RemovePolishSignsTest.java
+++ src/test/java/com/sda/general/ex04/RemovePolishSignsTest.java
@@ -9,15 +9,11 @@ class RemovePolishSignsTest {
     RemovePolishSigns removePolishSigns = new RemovePolishSigns();
 
     @Test
-    public void RemovePolishSigns(){
+    public void RemovePolishSigns() {
 
         String correctResult = "zolc";
         assertEquals(correctResult, removePolishSigns.reverseFromPolishToEnglishbySwitch("żółć"));
-        assertNotEquals("żolc",removePolishSigns.reverseFromPolishToEnglishbySwitch("żółć"));
+        assertNotEquals("żolc", removePolishSigns.reverseFromPolishToEnglishbySwitch("żółć"));
 
     }
-
-
-
-
 }
