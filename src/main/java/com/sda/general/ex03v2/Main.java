59e862ef195f97c74586a53bf5fe5a83c88197be
Marek Krzyszczyk <marek.krzyszczyk@wp.pl>
2019-10-27 23:17:22 +0100

    General ex03v2 convertFromJsonToObject finish

--- src/main/java/com/sda/general/ex03v2/Main.java
+++ src/main/java/com/sda/general/ex03v2/Main.java
@@ -1,10 +1,15 @@
 package com.sda.general.ex03v2;
 
+import com.sda.regularexpressions.ex03.TextInput;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
+        TextInput textInput = new TextInput();
 
         Person person = new Person();
         person.setName("Janusz");
@@ -14,21 +19,24 @@ public class Main {
 
         Person person2 = new Person();
         person2.setName("Janina");
-        person2.setSurname("Mędrzec");
+        person2.setSurname("Janusz");
         person2.setPhoneNumber("698965467");
         person2.setEMail("janina@wp.pl");
 
         List<Person> persons = new ArrayList<>();
         List<Person> personsFromJson;
+        List<Person> filtredPersons;
         persons.add(person);
         persons.add(person2);
 
         JsonFilesService jsonFilesService = new JsonFilesService();
-        jsonFilesService.insertToJsonFile(persons);
+        // jsonFilesService.insertToJsonFile(persons);
         personsFromJson = jsonFilesService.convertJSONToObject();
-        
-
-
+        System.out.println("Podaj szukaną frazę: ");
+        String text = textInput.getText();
+        filtredPersons = personsFromJson.stream().filter(p -> p.getName().equals(text) || p.getSurname().equals(text)
+                || p.getPhoneNumber().equals(text) || p.getEMail().equals(text)).collect(Collectors.toList());
+        System.out.println(filtredPersons);
     }
 
 }
