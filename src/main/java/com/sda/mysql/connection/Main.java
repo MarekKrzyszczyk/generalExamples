d5dfb08dfc22fd9e4265d0e84751f56eeb15e3f4
Marek Krzyszczyk <marek.krzyszczyk@wp.pl>
2019-11-16 14:16:41 +0100

    mySQL connections

---
+++ src/main/java/com/sda/mysql/connection/Main.java
@@ -0,0 +1,24 @@
+package com.sda.mysql.connection;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/exercises?autoReconnect=true&useSSL=false&useJDBCCompliantTimezoneShift=true&serverTimezone=UTC";
+        String user = "root";
+        String password = "Stasio17!";
+        try {
+            Connection myConnection = DriverManager.getConnection(url, user, password);
+            Statement myStatement = myConnection.createStatement();
+            String sql = "Select * from exercises.pracownicy";
+            ResultSet rs = myStatement.executeQuery(sql);
+            while(rs.next()){
+                System.out.println(rs.getString("imie")+" " + rs.getString("nazwisko"));
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
